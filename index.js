c51b89ef6d59bb77946ed1b61fec4ede32a89dc4
Jed Peek <jedpeek@yahoo.com>
2018-04-27 19:59:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 }// end function
 
 function placeOrder(cardNumber) {
-  if(cardNumber.length > 0){
+  if(cardNumber !== undefined){
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     }
      else{ return "Sorry, we don't have a credit card on file for you.";
