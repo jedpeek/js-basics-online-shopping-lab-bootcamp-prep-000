e5b13415733fca25620b5e2ad583c7d87c32e241
Jed Peek <jedpeek@yahoo.com>
2018-04-27 18:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,31 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push( {itemName:`${item}`,
+        itemPrice: Math.floor(Math.random()*100)
+ });
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+    if(cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  else if(cart.length === 1){
+   return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  else if(cart.length === 2){
+   return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }
+  else(cart.length > 2){
+    for(let i = 0; i > cart.length-2; i++){
+      `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, ${cart[i+1].itemName} at $${cart[i + 1].itemPrice}, and quince at $91.`;
+    }//for loop end
+  }//else end
+}// view cart end
 
 function total() {
-  // write your code here
+
 }
 
 function removeFromCart(item) {
