92f0ac717e61361adf9be75bfdc6864b383cf85f
Jed Peek <jedpeek@yahoo.com>
2018-04-27 19:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 }// end function
 
 function placeOrder(cardNumber) {
-  if(cardNumber.length() > 0){
+  if(cardNumber.length > 0){
     cart = [];
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     }
